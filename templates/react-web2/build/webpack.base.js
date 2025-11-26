487cc81b4afcd3ae11b59ecf5768b039357111f1
ytg <522125842@qq.com>
2025-11-26 17:28:29 +0800

    feat: optimize webpack dev&prod config

--- templates/react-web2/build/webpack.base.js
+++ templates/react-web2/build/webpack.base.js
@@ -1,22 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { experiments } = require("webpack");
 
-const resolvePath = (relativePath) => path.resolve(__dirname, '..', relativePath);
+const resolvePath = (relativePath) =>
+  path.resolve(__dirname, "..", relativePath);
 
 module.exports = {
-  entry: resolvePath('src/index.tsx'),
+  entry: resolvePath("src/index.tsx"),
   output: {
-    path: resolvePath('dist'),
-    filename: 'static/js/[name].[contenthash:8].js',
-    assetModuleFilename: 'static/media/[name].[hash][ext][query]',
-    publicPath: '/',
+    path: resolvePath("dist"),
+    filename: "static/js/[name].[contenthash:8].js",
+    assetModuleFilename:
+      "static/media/[name].[hash][ext][query]",
+    publicPath: "/",
     clean: true,
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
     alias: {
-      '@': resolvePath('src'),
+      "@": resolvePath("src"),
     },
+  },
+  // 启用异步WebAssembly
+  experiments: {
+    asyncWebAssembly: true,
   },
   module: {
     rules: [
@@ -24,31 +31,75 @@ module.exports = {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'swc-loader',
+          loader: "swc-loader",
           options: {
             jsc: {
-              parser: { syntax: 'typescript', tsx: true },
-              transform: { react: { runtime: 'automatic' } },
-              target: 'es2020'
-            }
-          }
-        }
+              parser: { syntax: "typescript", tsx: true },
+              transform: {
+                react: { runtime: "automatic" },
+              },
+              target: "es2020",
+            },
+          },
+        },
+      },
+      // 将SVG作为组件导入时，使用@svgr/webpack转换为React组件
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: /react/, // import Icon from './icon.svg?react'
+        use: ["@svgr/webpack"],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset',
+        test: /\.(png|jpe?g|gif|webp|avif)$/i,
+        type: "asset",
         parser: {
           dataUrlCondition: {
             maxSize: 8 * 1024,
           },
         },
       },
+      // 将SVG作为URL导入时，使用asset/inline处理
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        resourceQuery: { not: [/react/] }, // 普通 import 作为 URL
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+      },
+      // 字体
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/fonts/[name].[hash:8][ext]",
+        },
+      },
+      // 音视频
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/media/[name].[hash:8][ext]",
+        },
+      },
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async",
+      },
+      {
+        test: /\.(txt|md|glsl|vert|frag)$/i,
+        type: "asset/source", // 作为字符串导入
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: resolvePath('public/index.html'),
+      template: resolvePath("public/index.html"),
     }),
   ],
-  stats: 'minimal',
+  stats: "minimal",
 };
--- templates/react-web2/build/webpack.production.js
+++ templates/react-web2/build/webpack.production.js
@@ -1,33 +1,134 @@
-const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const baseConfig = require('./webpack.base');
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const {
+  BundleAnalyzerPlugin,
+} = require("webpack-bundle-analyzer");
+const baseConfig = require("./webpack.base");
+
+const isAnalyze = process.env.ANALYZE === "true";
 
 module.exports = merge(baseConfig, {
-  mode: 'production',
-  devtool: false,
+  mode: "production",
+
+  // 生产环境 source map（用于错误追踪，不暴露源码）
+  devtool: "hidden-source-map",
+
   output: {
-    filename: 'static/js/[name].[contenthash:8].js',
+    filename: "static/js/[name].[contenthash:8].js",
+    chunkFilename:
+      "static/js/[name].[contenthash:8].chunk.js", // 异步 chunk 命名
   },
+
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+        ],
       },
     ],
   },
+
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'static/css/[name].[contenthash:8].css',
+    // Source Map 输出到单独目录（dist 同级的 sourcemaps 目录）
+    new webpack.SourceMapDevToolPlugin({
+      filename: "../sourcemaps/[file].map",
+      append: false, // 不在 JS/CSS 中添加 sourceMappingURL 注释
     }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash:8].css",
+      chunkFilename:
+        "static/css/[name].[contenthash:8].chunk.css",
+    }),
+
+    // Gzip 压缩（nginx 也可以做，但预压缩更快）
+    new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240, // 大于 10KB 才压缩
+      minRatio: 0.8,
+    }),
+
+    // Brotli 压缩（比 gzip 更小，现代浏览器支持）
+    new CompressionPlugin({
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      filename: "[path][base].br",
+    }),
+
+    // Bundle 分析（运行 ANALYZE=true pnpm build）
+    ...(isAnalyze ? [new BundleAnalyzerPlugin()] : []),
   ],
+
   optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true, // 多线程压缩
+        terserOptions: {
+          compress: {
+            drop_console: true, // 删除 console
+            drop_debugger: true, // 删除 debugger
+            pure_funcs: ["console.log", "console.info"], // 删除指定函数
+          },
+          format: {
+            comments: false, // 删除注释
+          },
+        },
+        extractComments: false, // 不生成 LICENSE.txt
+      }),
+      new CssMinimizerPlugin(),
+    ],
+
+    // 代码分割策略
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
+      cacheGroups: {
+        // React 相关库单独打包
+        react: {
+          name: "react-vendor",
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+          priority: 20,
+          chunks: "all",
+        },
+        // 其他第三方库
+        vendors: {
+          name: "vendors",
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: "all",
+          minSize: 0,
+        },
+        // 公共模块
+        common: {
+          name: "common",
+          minChunks: 2, // 被引用 2 次以上
+          priority: 5,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    runtimeChunk: 'single',
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+
+    // Runtime 单独打包（利于缓存）
+    runtimeChunk: "single",
+
+    // 模块 ID 使用确定性算法（利于缓存）
+    moduleIds: "deterministic",
+  },
+
+  // 性能提示
+  performance: {
+    hints: "warning",
+    maxEntrypointSize: 250 * 1024, // 入口文件 250KB 警告
+    maxAssetSize: 250 * 1024, // 单个资源 250KB 警告
   },
 });
